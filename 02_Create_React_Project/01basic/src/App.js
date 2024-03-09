744db8c2354bdbbb9652597cc71989e2fe43319b
unstoppableayush <ayushkum884@gmail.com>
2024-03-09 21:03:29 +0530

    created create-react-app and vite-react , made hello world project

---
+++ 02_Create_React_Project/01basic/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <h1>Chai aur React | Ayush Kumar</h1>
+  );
+}
+
+export default App;
---
+++ 02_Create_React_Project/01vitereact/src/App.jsx
@@ -0,0 +1,10 @@
+
+
+function App() {
+
+  return (
+   <h1>Chai aur react with Vite | Ayush Kumar</h1>
+  )
+}
+
+export default App
